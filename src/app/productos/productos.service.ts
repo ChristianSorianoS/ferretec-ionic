c5276fe206a69dccc2f590450685ac275d3f0454
Christian Soriano <csorianos@gmail.com>
2023-05-17 22:25:46 -0400

    modificaciones

--- src/app/productos/productos.service.ts
+++ src/app/productos/productos.service.ts
@@ -10,13 +10,19 @@ export class ProductosService {
       id:'1',
       titulo:'Martillo',
       url : "https://easycl.vtexassets.com/arquivos/ids/364511/9008367.jpg?v=637533649361400000",
-      comentarios : ["prueba1", "prueba 2"]
+      comentarios : ["Martillo carpintero 0,45kg Robust"]
     },
     {
       id:'2',
       titulo:'Taladro',
       url : "https://www.akenergystore.cl/wp-content/uploads/2020/07/005.jpg",
-      comentarios : ["prueba1", "prueba 2"]
+      comentarios : ["Taladro Percutor Inalámbrico Dewalt Dcd778/batería 2ah 20v"]
+    },
+    {
+      id:'3',
+      titulo:'Set destornilladores',
+      url : "https://http2.mlstatic.com/D_NQ_NP_2X_682385-MLC42143204394_062020-F.webp",
+      comentarios : ["Juego 6 Destornilladores Con Soporte Stanley 60-060s"]
     }
 
   ];
